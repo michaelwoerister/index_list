701b9030492a13b2680ee2bda3228e8a64a29595
cargo-incremental <none>
2016-12-05 15:59:12 -0500

    checkpoint

--- src/lib.rs
+++ src/lib.rs
@@ -130,5 +130,13 @@ mod tests {
         assert_eq!(list.get(3), None);
     }
 
+    #[test]
+    fn non_immediate_len() {
+        for len in 0 .. 20 {
+            let mut reference: Vec<usize> = Vec::from(0..len);
+
+
+        }
+    }
 
 }
