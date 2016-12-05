2275280829c1ff4b7a669ebb769777595bd13f12
cargo-incremental <none>
2016-12-05 15:07:46 -0500

    checkpoint

--- src/lib.rs
+++ src/lib.rs
@@ -20,7 +20,7 @@ impl IndexList {
     }
 
     pub fn get(&self, index: usize) -> Option<usize> {
-        self.items[index]
+        self.items.get(index).map(|x| *x)
     }
 
     pub fn set(&mut self, index: usize, value: usize) {
