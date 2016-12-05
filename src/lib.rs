1125560d08cadae3cb881c42b1acac2e661a3274
cargo-incremental <none>
2016-12-05 16:14:09 -0500

    checkpoint

--- src/lib.rs
+++ src/lib.rs
@@ -102,7 +102,10 @@ impl IndexList {
 impl Drop for IndexList {
     fn drop(&mut self) {
         if !self.is_immediate() {
-            Box::from_raw(self.ptr_or_list as *mut Vec<usize>);
+            unsafe {
+                // Drop the boxed vec
+                let _ = Box::from_raw(self.ptr_or_list as *mut Vec<usize>);
+            }
         }
     }
 }
